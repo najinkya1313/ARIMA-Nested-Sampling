abc0b2524d4e9e3b548f8f0a5e012cd23c5dc819
Ajinkya Jayant Naik <114169321+najinkya1313@users.noreply.github.com>
2025-07-18 17:35:55 +0100

    Create ARIMA_func.py

---
+++ ARIMA_func.py
@@ -0,0 +1,60 @@
+##Defining an ARIMA function from scratch
+def ARIMA_custom(data,order:tuple,sigma:int,phi:tuple,theta:tuple):
+ "A non-seasonal ARIMA function that returns the forecasted values y_t for a given time-series data"   
+ 
+ noise_key = jax.random.PRNGKey(18)
+ epsilon_t = sigma * jax.random.normal(noise_key, shape=data.shape) #Error/Shock at time t
+ p,d,q = order[0],order[1],order[2]
+ 
+ ##Converting inputs into jax numpy arrays
+ phi = jnp.atleast_1d(phi)
+ theta = jnp.atleast_1d(theta)
+ data = jnp.array(data)
+ 
+ ##Defining the constant intercept k
+ phi_sum = jnp.sum(phi)
+ k = (1-phi_sum)*jnp.mean(diff_data)
+ 
+ ## Differencing for stationarity
+ diff_data = jnp.diff(data,d)
+ ##Padding with zeroes for first forecast
+ if p>q:
+   diff_data = jnp.concatenate((np.zeros(p),data)) 
+ else:
+   diff_data = jnp.concatenate((np.zeros(q),data))
+ 
+ ## Autoregression and MA part:
+ y_t_diffed = q*[0]
+ k = (1-phi_sum)*jnp.mean(diff_data)
+ for index,val in enumerate(diff_data):
+    if index<(len(diff_data)-p):
+        y_t=[]
+        iter_var_ar=0
+        while iter_var_ar<p:
+            y = phi[iter_var_ar]*diff_data[index+iter_var_ar] 
+            y_t.append(y)
+            iter_var_ar+=1
+        
+        iter_var_ma = 0
+        while iter_var_ma<q:
+           error = diff_data[index+iter_var_ma] - y_t_diffed[index+iter_var_ma]
+           y_t.append(theta[iter_var_ma]*error)
+           iter_var_ma+=1
+        y_t = jnp.array(y_t)
+        y_t_diffed = list(y_t_diffed)
+        y_t_diffed.append(k+jnp.sum(y_t))
+        y_t_diffed = jnp.array(y_t_diffed)
+
+ ##Integrating the forecasts to match the original time-series:
+ if d==0:
+     integrated = y_t_diffed
+    
+ else:
+     integrated = jnp.asarray(y_t_diffed).copy()
+     initial_values = data[:d]   
+     for i in range(d, 0, -1):
+      first_val = jnp.diff(initial_values, n=i-1)[0]
+      integrated = jnp.concatenate(([first_val], jnp.cumsum(integrated) + first_val))
+ 
+ y_t_out = integrated + epsilon_t  
+ return y_t_out
