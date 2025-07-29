66b871a2752cb4d7392c63d9349fdc98bcd183f3
Ajinkya Jayant Naik <114169321+najinkya1313@users.noreply.github.com>
2025-07-29 17:45:54 +0100

    Update ARIMA_Nested_Sampler.py

--- ARIMA_Nested_Sampler.py
+++ ARIMA_Nested_Sampler.py
@@ -92,9 +92,10 @@ class Nested_Sampler_ARIMA:
     return posterior_samples
      #Print results:
  def summary(self,posterior_samples):
-    print("NESTED SAMPLING SUMMARY RESULTS :")
+    print("||NESTED SAMPLING SUMMARY RESULTS||")
     print("---------------------------------------------------")
     print(f"Nested sampling runtime: {self.ns_time:.2f} seconds")
+    print("---------------------------------------------------")
     posterior_means = []
     for key in self.prior_bounds.keys():
      means = posterior_samples[key].mean()
@@ -103,6 +104,7 @@ class Nested_Sampler_ARIMA:
      print("---------------------------------------------------")
     
     print(f"Log Evidence: {posterior_samples.logZ():.2f} ± {posterior_samples.logZ(100).std():.2f}")
+    print("-------x----------------x-------------x------------")
     Z = posterior_samples.logZ()
     
     
