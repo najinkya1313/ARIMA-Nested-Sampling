d18820780b9ea96b4390fc1d212ea0c40a3a6890
Ajinkya Jayant Naik <114169321+najinkya1313@users.noreply.github.com>
2025-09-23 13:06:33 +0530

    Update model_comparison.py

--- model_comparison.py
+++ model_comparison.py
@@ -17,7 +17,6 @@ def ARIMA_model_comparison(data,orders,prior_type,num_live,num_delete,seeds,scal
         order_done.append(order)
         evidence_arr = np.array(evidences)
         index = np.where(evidence_arr==max(evidence_arr))[0][0]
-        print(index)
         print("----------------------x-------------------x---------------------x------")
         print(f"Evidence : {model.log_evidence} ; Error : {model.log_evidence_err}")
         print(f"Highest Evidence so far : {max(evidences)} for order : {order_done[index]}")
