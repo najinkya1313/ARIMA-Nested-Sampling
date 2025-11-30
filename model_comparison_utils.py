00328699f4be7c63e879f70364e8b2c950c9b8c6
Ajinkya Jayant Naik <114169321+najinkya1313@users.noreply.github.com>
2025-11-30 14:19:01 +0530

    Add axes parameter to plot_evidence_heatmap function

--- model_comparison_utils.py
+++ model_comparison_utils.py
@@ -85,7 +85,7 @@ def load_evidence_file(file_name):
     return model_posteriors
 
 
-def plot_evidence_heatmap(data, max_order, contrast=0, highlight_max=True,annotate=True,invert=False, **kwargs):
+def plot_evidence_heatmap(data, max_order, contrast=0, highlight_max=True,annotate=True,invert=False,axes, **kwargs):
     """Plots a heatmap of the model log posterior probabilities and their associated errors on a grid of ARMA models.
     Arguments:
     data : the list of model log posterior probabilities and their associated error passed as a tuple (model_log_P,error)
@@ -115,6 +115,7 @@ def plot_evidence_heatmap(data, max_order, contrast=0, highlight_max=True,annota
     width = kwargs.get('fig_width',fig_width)
     height = kwargs.get('fig_height',fig_height)
     fig, ax = plt.subplots(figsize=(width, height))
+    ax = axes
     if invert:
 
      im = ax.imshow(heatmap_data, origin="lower", cmap="inferno_r", vmin=vmin, vmax=vmax)
